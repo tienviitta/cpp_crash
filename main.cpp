74fdecf3e398b213a1ccb5c8ebf3c44a4db5f88c
Väisänen, Petri <Petri.Vaisanen@nordicsemi.no>
2022-12-14 17:28:00 +0200

    CMakeLists bug fix and draft Armadillo code added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <armadillo>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+using namespace std;
+using namespace arma;
+
+int main() {
+
+    cout << "Armadillo:" << endl;
+
+    mat A(4, 5, fill::randu);
+    mat B(4, 5, fill::randu);
+
+    cout << A << "*" << endl << B.t() << "=" << endl;
+
+    cout << A*B.t() << endl;
+
+    return 0;
 }
