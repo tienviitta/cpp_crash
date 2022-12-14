5f92e4e023c85abd9e02c139faf756141e8b2de6
Väisänen, Petri <Petri.Vaisanen@nordicsemi.no>
2022-12-14 20:40:53 +0200

    Some complex signals and resource allocs

--- main.cpp
+++ main.cpp
@@ -5,15 +5,38 @@ using namespace std;
 using namespace arma;
 
 int main() {
-
+    // Armadillo testing
     cout << "Armadillo:" << endl;
 
-    mat A(4, 5, fill::randu);
-    mat B(4, 5, fill::randu);
+    // // Matrix mul:
+    // mat A(4, 5, fill::randu);
+    // mat B(4, 5, fill::randu);
+    // cout << A << "*" << endl << B.t() << "=" << endl;
+    // cout << A*B.t() << endl;
 
-    cout << A << "*" << endl << B.t() << "=" << endl;
+    // // Vec
+    // cx_rowvec x(10, fill::ones);
+    // cx_rowvec n(10, fill::randu);
+    // cout << x << "+" << endl << n << "=" << endl << x+n << endl;
 
-    cout << A*B.t() << endl;
+    // Complex symbols
+    vec data_re = sign(randn(16))/sqrt(2);
+    vec data_im = sign(randn(16))/sqrt(2);
+    cx_vec data(data_re, data_im);
+    cout << data << endl;
 
+    // Indices and elements
+    imat RA(8, 7, fill::zeros);
+    cx_mat RE(8, 7, fill::zeros);
+    RA(span(2, 5), span(0, 2)).fill(1);    
+    RA(span(2, 5), span(3, 6)).fill(2);    
+    cout << RA << endl;
+    uvec idx_cc = find(RA == 1);
+    RE.elem(idx_cc).fill(cx_double(1.0, 1.0)/sqrt(2.0));
+    uvec idx_dc = find(RA == 2);
+    RE.elem(idx_dc) = data;
+    cout << size(data) << endl << size(idx_dc) << endl << RE << endl;
+
+    // ...
     return 0;
 }
